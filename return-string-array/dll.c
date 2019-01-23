0becfa044ab6c3d41f5274c58730d230551dd2e1
René Nyffenegger <mail@renenyffenegger.ch>
2019-01-23 13:40:07 +0100

    + dll.def

--- return-string-array/dll.c
+++ return-string-array/dll.c
@@ -1,3 +1,9 @@
+//
+//   This DLL must be linked to OleAut32:
+//
+//       cl /LD dll.c oleAut32.lib /Fethe.dll /link /def:dll.def
+//
+
 #include <windows.h>
 
 __declspec(dllexport)  SAFEARRAY* __stdcall stringArray() {
@@ -5,17 +11,18 @@ __declspec(dllexport)  SAFEARRAY* __stdcall stringArray() {
  // Specifiy the bounds of ONE dimenision of the SAFEARRAY:
     SAFEARRAYBOUND dim;
 
+    SAFEARRAY     *ret;
+    LONG           index = 0;
+
     dim.lLbound   = 0; // We want the array to start with index 0
     dim.cElements = 3; // We return 3 values
 
-    SAFEARRAY *ret;
     ret = SafeArrayCreate(
          VT_BSTR,
          1      , // Array to be returned has one dimension …
         &dim      // … which is described in this (dim) parameter
     );
 
-    LONG index = 0;
 
     SafeArrayPutElement(ret, &index, SysAllocString(L"foo")); index ++;
     SafeArrayPutElement(ret, &index, SysAllocString(L"bar")); index ++;
